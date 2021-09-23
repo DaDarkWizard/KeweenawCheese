856a1a62c75161231020678580fb400000c6f6a9
DaDarkWizard <danimask@ilc.edu>
2021-09-23 17:22:55 -0400

    Production now automatically shows under custruction

--- KeweenawCheeseSite/Pages/Index.cshtml.cs
+++ KeweenawCheeseSite/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +13,22 @@ namespace KeweenawCheeseSite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _env;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
             var user = User.Claims;
+            if (_env.IsProduction())
+            {
+                return RedirectToPage("UnderConstruction");
+            }
+            return Page();
         }
     }
 }
